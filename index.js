39ea7732c10696799e5473f2c468cd24e58d67f5
Yan <ymatheus706@gmail.com>
2022-12-21 00:45:30 -0300

    modificaçao no index

--- index.js
+++ index.js
@@ -9,12 +9,15 @@ var rl = readline.createInterface({
 const url = "https://pokemon.fandom.com/pt-br/wiki/Pok%C3%A9dex_Nacional"; // url da página solicitada
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: true }); // busca os dados da página
-    const page = await browser.newPage();
-    await page.goto(url); // busca a url da página
+    const browser = 
+      await puppeteer.launch({ headless: true }); // busca os dados da página
+    const page = 
+      await browser.newPage();
+        await page.goto(url); // busca a url da página
   
     const data = await page.evaluate(() => {
-      const nome = Array.from(document.querySelectorAll('td')) // coleta todas as tags td onde obtem as informaçoes dos tipos do pokemon
+      const nome = 
+        Array.from(document.querySelectorAll('td')) // coleta todas as tags td onde obtem as informaçoes dos tipos do pokemon
 
 
       return nome.map(td => td.innerText) // retorna um array com os tipos das tabelas 
@@ -25,7 +28,9 @@ const url = "https://pokemon.fandom.com/pt-br/wiki/Pok%C3%A9dex_Nacional"; // ur
       for(let i = 0; i < data.length; i++){
         if(data[i] == busca){ // seleciona o tipo do pokemon que você solicitou
 
-            console.log(`O Pokemon ${busca} é do tipo${data[i+1]} `)
+            console.log(
+              `O Pokemon ${busca} é do tipo${data[i+1]} `
+              )
             return;
         }
     }
