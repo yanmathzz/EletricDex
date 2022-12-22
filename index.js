4abc046ff551094c9da34dd7034039eb9906ff70
Yan <ymatheus706@gmail.com>
2022-12-21 21:02:21 -0300

    alteraçoes

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
-const puppeteer = require('puppeteer');
-var readline = require('readline');
+const puppeteer = require("puppeteer");
+var readline = require("readline");
 
 var rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 const url = "https://pokemon.fandom.com/pt-br/wiki/Pok%C3%A9dex_Nacional"; // url da página solicitada
 
 (async () => {
-    const browser = 
-      await puppeteer.launch({ headless: true });                         // busca os dados da página
+  const navegador = await puppeteer.launch({ headless: true }); // busca os dados da página
 
-    const page = 
-      await browser.newPage();
-        await page.goto(url);                                             // busca a url da página
-  
-    const data = await page.evaluate(() => {
-      const nome = 
-        Array.from(document.querySelectorAll('td'))                       // coleta todas as tags td onde obtem as informaçoes dos tipos do pokemon
+  const pagina = await navegador.newPage();
+  await pagina.goto(url); // busca url da página
 
-      return nome.map(td => td.innerText)                                 // retorna um array com os tipos das tabelas 
+  const data = await pagina.evaluate(() => {
+    const nome = Array.from(document.querySelectorAll("td")); // coleta todas as tags td onde obtem as informaçoes dos tipos do pokemon
 
-    });
+    return nome.map((td) => td.innerText); // retorna um array com os tipos das tabelas
+  });
 
-    rl.question("Qual pokemon você deseja pesquisar? ", function(busca) { // procura o pokemon que voce deseja saber 
+  rl.question("Qual pokemon você deseja pesquisar na sua EletricDex? ", function (busca) {
+    // procura o pokemon que voce deseja saber
 
-      for(
-        let i = 0; i < data.length; i++
-        ){
+    busca = busca.toLowerCase();
 
-        if(data[i] == busca){                                             // seleciona o tipo do pokemon que você solicitou
-
-            console.log(
-              `O Pokemon ${busca} é do tipo${data[i+1]} `
-              )
-            return;
-        }
-    }
+    const firstLetter = busca.charAt(0);
     
-  rl.close();                                                             // fecha o terminal 
-  }); 
+    const firstLetterCap = firstLetter.toUpperCase();
 
-  await page.waitForTimeout(3000);                                        // tempo de busca da informaçao solicitada 
-  await browser.close();
-  
+    const remainingLetters = busca.slice(1);
+
+    const buscaCap = firstLetterCap + remainingLetters;
+
+    for (let i = 0; i < data.length; i++) {
+      if (data[i] == buscaCap) {
+        // seleciona o tipo do pokemon que você solicitou
+
+        console.log(`O Pokemón pesquisado ${buscaCap} é do tipo${data[i + 1]}`);
+        return;
+      }
+    }
+
+    rl.close(); // fecha o terminal
+  });
+
+  await pagina.waitForTimeout(3000); // tempo de busca da informaçao solicitada
+  await navegador.close(); // fecha o navegador
 })();
