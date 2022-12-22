0d14d75a124c2a54b864c1b3e25e55a19efd800a
Yan <ymatheus706@gmail.com>
2022-12-22 01:05:33 -0300

    modificação no index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const url = "https://pokemon.fandom.com/pt-br/wiki/Pok%C3%A9dex_Nacional"; // ur
   const pagina = await navegador.newPage();
     await pagina.goto(url); // busca url da página
 
-  const data = await pagina.evaluate(() => {
+  const dados = await pagina.evaluate(() => {
     const tipagem = Array.from(document.querySelectorAll("td")); // coleta todas as tags td, onde obtem as informaçoes dos tipos do pokemon
 
     return tipagem.map((td) => td.innerText); // retorna um array com os tipos das tabelas
@@ -24,20 +24,20 @@ const url = "https://pokemon.fandom.com/pt-br/wiki/Pok%C3%A9dex_Nacional"; // ur
     // procura o pokemon que voce deseja saber
 
     pesquisar = pesquisar.toLowerCase();
-
-    const firstLetter = pesquisar.charAt(0);
     
-    const firstLetterCap = firstLetter.toUpperCase();
+      const firstLetter = pesquisar.charAt(0);
+    
+      const firstLetterCap = firstLetter.toUpperCase();
 
-    const remainingLetters = pesquisar.slice(1);
+      const remainingLetters = pesquisar.slice(1);
 
-    const pesquisarCap = firstLetterCap + remainingLetters;
+      const pesquisarCap = firstLetterCap + remainingLetters;
 
-    for (let i = 0; i < data.length; i++) {
-      if (data[i] == pesquisarCap) {
+    for (let i = 0; i < dados.length; i++) {
+      if (dados[i] == pesquisarCap) {
         // seleciona o tipo do pokemon que você solicitou
 
-        console.log(`O Pokemón pesquisado ${pesquisarCap} é do tipo${data[i + 1]}`);
+        console.log(`O Pokemón pesquisado ${pesquisarCap} é do tipo${dados[i + 1]}`);
         return;
       }
     }
